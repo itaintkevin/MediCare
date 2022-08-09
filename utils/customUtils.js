f5ea64e8f8475940c5e32f3f11659f9d1b3cd4c3
Kunal Keshan <kunalkeshan12@gmail.com>
2022-08-09 09:46:39 +0530

    update in utils

--- utils/customUtils.js
+++ utils/customUtils.js
@@ -3,13 +3,15 @@
  */
 
 class ApiError extends Error {
-    constructor({message = '', statusCode = 500, data = {}}){
+    constructor({ message = '', statusCode = 500, data = {} }) {
         super();
         this.message = message;
         this.statusCode = statusCode;
         this.data = data;
     };
 };
+
+const verifyDoctorRegNo = async () => { };
 
 module.exports = {
     ApiError,
